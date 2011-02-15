d32b934685a61ca386d0cf57184a3c375a9801bf
Regan <rroyste2@mail.umw.edu>
2011-02-15 16:46:50 -0500

    I tried doing the dangerous characters..someone help?

--- search.php
+++ search.php
@@ -61,6 +61,9 @@
   	}
   	
   }
+  $movie = mysqli_real_escape_string($db, trim($_POST['movieName'));
+  $theater = mysqli_real_escape_string($db, trim($_POST['theater'));
+  $query = "INSERT INTO movies VALUES ($movie, $theater)";
   ?>
   <p>&nbsp;</p><p><a href="logout.php">logout</a></p>
   </div>
