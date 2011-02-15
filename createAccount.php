f0ef3a7e0fe2c25bde6cbdb40274c8d6176e7a7e
Gary M <gmills@mail.umw.edu>
2011-02-15 14:16:22 -0500

    first try

---
+++ createAccount.php
@@ -0,0 +1,43 @@
+<?php 
+ session_start();
+?>
+<?php
+ $_session ['username'] = '$username';
+$_session ['pw'] = '$pw';
+echo "logged in as ". $_session['username'];
+ ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Log In</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+  <form method="post" action="login.php">
+    
+   
+	<input type="submit" value="Click to continue" name="submit" />
+	
+	
+  <?php
+
+include('db_connect.php');
+
+
+$username = $_POST['username'];
+$pw = $_POST['pw'];
+$zip = $_POST['zip'];
+
+?>
+<?php
+$query= "INSERT INTO users (id, username, password, zipcode) VALUES(NULL, '$username', SHA('$pw'), '$zip')";
+$result = mysqli_query($db, $query)
+or die("Error Querying Database");
+mysqli_close($db);
+
+
+?>
+
---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Log In</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+<div id="contents">
+<?php
+
+  include "db_connect.php";
+  $name = $_POST['username'];
+  $pw = $_POST['pw'];
+
+   $query = "select * from users WHERE username = '$name' AND password = '$pw'";
+   $result = mysqli_query($db, $query);
+   if ($row = mysqli_fetch_array($result))
+   {
+   		$_SESSION['zip'] = $row['zipcode'];
+   		echo "<p>Thanks for logging in, $name</p>\n";
+   		echo "<p><a href=\"search.php\">Continue</a></p>";
+   }
+   else
+    {
+   		echo "<p>Incorrect username or password</p>\n";
+   		echo  "<h1>Log In</h1>\n  <form method=\"post\" action=\"login.php\">";
+    	echo "<label for=\"username\">Username:</label><input type=\"text\" id=\"username\" name=\"username\" /><br />";
+        echo "<label for=\"pw\">Password:</label><input type=\"password\" id=\"pw\" name=\"pw\" /><br />";
+        echo "<input type=\"submit\" value=\"Login\" name=\"submit\" /></form> <p><a href=\"createAccount.html\">Create Account</a></p>";
+    }
+   
+  $_SESSION ['username'] = '$name';
+$_SESSION ['pw'] = '$pw';
+echo "logged in as ". $_session['username'];
+?>
+  
+  </div>
+</body>
+</html>
